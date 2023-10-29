e78ef63505966b3b2a9370d6d0b438bb345a5b47
celestialpivot <71176631+celestialpivot@users.noreply.github.com>
2023-10-29 21:27:51 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -7,8 +7,6 @@
 const MMSLS = [
     
     "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
   ]
 =====================以上为范例！=====================
 
