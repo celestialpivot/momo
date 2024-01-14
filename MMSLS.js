b22f6867547594ccdb0272f4025f3146a9bcbc58
celestialpivot <71176631+celestialpivot@users.noreply.github.com>
2024-01-14 12:18:17 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -6,7 +6,7 @@
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=29875517&pid=6f0080f787b9a917c991bba747faaeff&tid=89e34d8258d5155fb442e4f3d0f138f1",
+    "https://www.maimemo.com/share/page?uid=29875517&pid=f16a312204f74d4886bc508e08af3963&tid=36e4e2868cd1955bad8aa3633267c4cd",
     "https://www.maimemo.com/share/page?uid=29875517&pid=0299371d0e981cff986b69646fda1e1b&tid=8496b6a2b1370f2840a914fd744a1957"
   ]
   /**
