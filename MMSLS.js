276111b784de67f7f8f9bc79e597ca6907c592a9
celestialpivot <71176631+celestialpivot@users.noreply.github.com>
2024-01-17 11:52:46 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -7,7 +7,7 @@
 const MMSLS = [
     
     "https://www.maimemo.com/share/page?uid=29875517&pid=662f5e00a288c231a644e71024835ef0&tid=f9fbb00e546030d88d3c27991784695f",
-    "https://www.maimemo.com/share/page?uid=29875517&pid=b02459eebd97b718c833ba0a963c89c1&tid=c935cc00c552fb337cd91f9bac04140c"
+    "https://www.maimemo.com/share/page?uid=29875517&pid=fd7d3e7267b739fe7845d45676917685&tid=c33a5faafe8f248df9d70a2b58bf3c75"
   ]
   /**
    * 生成随机数字
